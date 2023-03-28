b81614016b198eaf983f7cc251aa4b2eb6cd3b56
Bhavika0205 <81350206+Bhavika0205@users.noreply.github.com>
2023-03-28 22:27:02 +0530

    Create Boats_to_save_people.java

---
+++ Boats_to_save_people.java
@@ -0,0 +1,12 @@
+public int boats(int[] nums, int limit){
+    Arrays.sort(people);
+	int i=0;
+	int j=people.length-1;
+	int ans =0;
+	while(i<=j){
+	    ans++;
+		if(people[i] + people[j] <=limit) i++;
+		j--;
+	}
+	return ans;
+}
